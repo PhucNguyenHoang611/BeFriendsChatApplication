363327e79e767aa3d40880a130a4a578067efc50
PhucNguyenHoang <hoangphucnguyen112002@gmail.com>
2024-04-03 15:19:52 +0700

    Change api url

--- client/src/api/mainApi.js
+++ client/src/api/mainApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const mainApi = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://befriends-api.onrender.com/api" // http://localhost:5000/api
 });
 
-export const hostURL = "http://localhost:5000"
-export const baseURL = "http://localhost:5000/api";
+export const hostURL = "https://befriends-api.onrender.com"
+export const baseURL = "https://befriends-api.onrender.com/api";
